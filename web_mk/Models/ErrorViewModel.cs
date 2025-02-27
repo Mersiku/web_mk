4af24713a206a78d611aa1af524c5b8ec7a8730b
112 <112@112-18>
2025-02-27 08:27:50 +0100

    Dodaj pliki projektów.

---
+++ web_mk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_mk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
