f62840091fa19cd3828b0fd338ce1ac80d85504a
Mersiku <marcel.kuras@wp.pl>
2025-04-10 09:25:53 +0200

    add Films

--- web_mk/Controllers/FilmsController.cs
+++ web_mk/Controllers/FilmsController.cs
@@ -1,16 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using web_mk.DAL;
+using web_mk.Models;
 
 namespace web_mk.Controllers
 {
     public class FilmsController : Controller
     {
         FilmsContext db;
+        IWebHostEnvironment webHost;
 
-        public FilmsController(FilmsContext db)
+
+        public FilmsController(FilmsContext db, IWebHostEnvironment webHost)
         {
             this.db = db;
+            this.webHost = webHost;
+        }
+
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            AddViewModel model = new AddViewModel();
+            model.AllCategories = db.Categories.ToList();
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public IActionResult AddFilm(AddViewModel model)
+        {
+            var posterFolderPath = Path.Combine(webHost.WebRootPath, "posters");
+            var uniquePosterName = model.Poster.FileName + "_" + Guid.NewGuid().ToString() + Path.GetExtension(model.Poster.FileName);
+
+            var filePath = Path.Combine(posterFolderPath, uniquePosterName);
+            model.Poster.CopyTo(new FileStream(filePath, FileMode.Create));
+
+            model.NewFilm.Poster = uniquePosterName;
+            db.Films.Add(model.NewFilm);
+            db.SaveChanges();
+            return RedirectToAction("AddFilm");
         }
 
         public IActionResult FilmsList(string categoryName)
---
+++ web_mk/Models/AddViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace web_mk.Models
+{
+    public class AddViewModel
+    {
+        public Film NewFilm { get; set; }
+
+        public IFormFile Poster { get; set; }    
+
+        public List <Category> AllCategories { get; set; }  
+    }
+}
